eb56b7ee8fe4af2f4998fd8b53b0a740e02c58a2
Jose Manuel Chavez Rodriguez <esojtec@hotmail.com>
2016-07-04 18:31:57 -0500

    Actualizacion clase Message

--- src/Libraries/Message.php
+++ src/Libraries/Message.php
@@ -2,19 +2,39 @@
 
 namespace Esojtec\Messages\Libraries;
 
+use Illuminate\Session\Store;
+
 class Message {
 
+        var $_name_session = 'esojtec.messages';
 	var $_messages = [];
+        var $_session = [];
 
-	function __Construct(){
-
+	function __Construct(Store $session){
+            $this->_session = $session;
 	}
 
 	function message($message, $type = 'info') {
-		$this->_messages[] = ['message' => $message, 'type' => $type];
+		$this->_session->put($this->_name_session,['message' => $message, 'type' => $type]);
 	}
 
+        function success($message){
+            $this->message($message, 'success');
+        }
+        
+        function error($message){
+            $this->message($message, 'danger');
+        }
+        
+        function warning($message){
+            $this->message($message, 'warning');
+        }
+        
+        function info(){
+            $this->message($message, 'info');
+        }
+        
 	function render() {
-		return $this->_messages;
+		return $this->_session->get($this->_name_session);
 	}
 }
